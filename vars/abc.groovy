46de25c75465df5742824577dab67367e5c2ac66
VinayTech2210 <vinaykandregula10@gmail.com>
2025-12-25 12:30:59 +0530

    Update abc.groovy

--- vars/abc.groovy
+++ vars/abc.groovy
@@ -1,1 +1,15 @@
 
+def gitdownload(repo)
+{
+git "https://github.com/IntelliqDevops/${repo}.git"
+}
+def gitbuildArtifact()
+{
+  sh 'mvn package'
+}
+def DeployementTomcat(jobname,ipaddress,contextpath)
+{
+
+sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war azureuser@${ipaddress}:/var/lib/tomcat10/webapps/${contextpath}.war"
+  
+}
